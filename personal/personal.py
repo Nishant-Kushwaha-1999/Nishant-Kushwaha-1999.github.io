d99c6630e6aee7dd80b505994c52fa000f9c2ce3
mostwanted-786 <60353780+nishantkushwaha-1999@users.noreply.github.com>
2023-12-08 17:54:56 -0600

    changes

---
+++ personal/personal.py
@@ -0,0 +1,21 @@
+import time from life
+
+time.heal()
+
+while alive:
+    eat()
+    sleep()
+    code()
+
+    if isWeekend:
+        takeBreak()
+
+
+while ~success:
+    if fail:
+        improvemnt += 1
+    if improvement > fear:
+        success = true
+
+
+os.execute(Select memories from life where type like 'joyfull')
